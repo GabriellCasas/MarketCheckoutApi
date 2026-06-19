03bd8c7758fdaf7a213fb095f174707f0f305a6c
Gabriel <gabrielcasas527@gmail.com>
2026-06-18 23:49:38 -0300

    Criação da estrutura do projeto e entidades

---
+++ Domain/Entities/Carrinho.cs
@@ -0,0 +1,11 @@
+﻿namespace MarketCheckoutApi.Models
+{
+    public class Carrinho
+    {
+        public int Id { get; set; }
+        public string NomeComprador { get; set; }
+        public string CpfComprador { get; set; }
+        public string CriadoPor { get; set; }
+        public DateTime DataCriacao { get; set; }
+    }
+}
---
+++ Domain/Entities/ItemCarrinho.cs
@@ -0,0 +1,10 @@
+﻿namespace MarketCheckoutApi.Models
+{
+    public class ItemCarrinho
+    {
+        public int Id { get; set; }
+        public int CarrinhoId { get; set; }
+        public int ProdutoId { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
